b86eeca7fb8446f44d99236d81fe553af079f9d5
kamalafidele <kamalafizzet45@gmail.com>
2023-03-05 18:54:12 +0200

    initializing

---
+++ index.js
@@ -0,0 +1,56 @@
+const QRCode = require('qrcode');
+const prompt = require('prompt-sync')();
+
+const generateToImage = async () => {
+    try {
+        const text = prompt('Enter the text: ');
+        await QRCode.toFile('./qr-code.png', text);
+        console.log('QR CODE generated successfully');
+    } catch (e) {}
+}
+
+const generateToTerminal = async () => {
+    try {
+        const text = prompt('Enter the text: ');
+        const code = await QRCode.toString(text, { type: 'terminal' });
+        console.log(code);
+        console.log('QR CODE generated successfully');
+    } catch (e) {}
+}
+
+const generateQRCode = async () => {
+    let choice = 0;
+
+    do {
+        console.log('1. Generate to image');
+        console.log('2. Generate to terminal');
+        console.log('3. EXIT THE PROGRAM');
+
+        choice = prompt('CHOOSE AN OPTION: ');
+
+        switch(parseInt(choice, 10)) {
+            case 1:
+                await generateToImage();
+                choice = 0;
+            break;
+            case 2:
+                await generateToTerminal();
+                choice = 0;
+            break;
+            case 3:
+                choice = 0;
+            break;
+            default:
+                console.log('Please select a correct option');
+        }
+    } while (choice !== 0);
+    // try {
+    //     await QRCode.toFile('./code.png', text);
+    //     const code = await QRCode.toString(text, { type: 'terminal' });
+    //     console.log(code);
+    // } catch (e) {
+    //     console.log(e);
+    // }
+}
+
+generateQRCode();
